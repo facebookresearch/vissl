84ddcd8dd07f1bb2f2717c2f889141b20525e617
Priya Goyal <prigoyal@fb.com>
2020-07-08 11:32:58 -0700

    Auto scale LR as resource requirement changes for a model (#29)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/vissl/pull/29
    
    infer the LR and adjust it based on the `base_value` and `base_lr_batch_size`. Users can turn off automatic LR scaling as well. In case of a composite schedule that involves linear warmup, we check if the warmup becomes meaningless and remove it.
    
    imisra, would be helpful to get your thoughts on this. e
    
    Reviewed By: imisra
    
    Differential Revision: D22433012
    
    fbshipit-source-id: c47c852bf8323fa604b6a5e9f9496d2ca3b07b27

--- dev/README.md
+++ dev/README.md
@@ -1,6 +1,6 @@
 ## Run everything on 1 gpu `low_resource_1gpu_train_wrapper.sh`
 
-If you have a configuration file (any vissl compatible file) that you want to run on 1-gpu only (for example: train SimCLR on 1 gpu, etc), you don't need to modify the config file. You can simple achieve this by using the `low_resource_1gpu_train_wrapper.sh` script. An example usage:
+If you have a configuration file (any vissl compatible file) that you want to run on 1-gpu only (for example: train SimCLR on 1 gpu, etc), you don't need to modify the config file. VISSL also takes care of auto-scaling the Learning rate for various schedules (cosine, multistep, step etc.) if you have enabled the auto_scaling (see `config.OPTIMIZER.param_schedulers.lr.auto_lr_scaling`). You can simply achieve this by using the `low_resource_1gpu_train_wrapper.sh` script. An example usage:
 
 ```bash
 cd $HOME/vissl
--- vissl/utils/hydra_config.py
+++ vissl/utils/hydra_config.py
@@ -66,6 +66,83 @@ def print_cfg(cfg):
         logging.info(pprint.pformat(cfg))
 
 
+def resolve_linear_schedule(cfg, param_schedulers):
+    # compute what should be the linear warmup start LR value.
+    # this depends on batchsize per node.
+    num_nodes = cfg.DISTRIBUTED.NUM_NODES
+    num_gpus_per_node = cfg.DISTRIBUTED.NUM_PROC_PER_NODE
+    bs_per_gpu = cfg.DATA.TRAIN.BATCHSIZE_PER_REPLICA
+    batch_size_per_node = bs_per_gpu * num_gpus_per_node
+    base_lr = param_schedulers.auto_lr_scaling.base_value
+    base_lr_batch_size = param_schedulers.auto_lr_scaling.base_lr_batch_size
+    scale_factor = float(batch_size_per_node) / base_lr_batch_size
+    start_value = base_lr * scale_factor
+
+    remove_linear_idx = -1
+    for idx in range(len(param_schedulers["schedulers"])):
+        if param_schedulers["schedulers"][idx]["name"] == "linear":
+            param_schedulers["schedulers"][idx]["start_value"] = start_value
+            if num_nodes == 1:
+                end_value = param_schedulers["schedulers"][idx]["end_value"]
+                if start_value <= end_value:
+                    # linear schedule is not meaningful as linear warmup is not needed.
+                    remove_linear_idx = idx
+
+    # check if linear warmup should be removed as its not meaningul
+    if remove_linear_idx >= 0:
+        del param_schedulers["schedulers"][remove_linear_idx]
+    # if after removing linear warmup, there's only one scheduler, then a composite
+    # schedule is no longer needed. The remaining scheduler becomes the primary
+    # scheduler
+    if len(param_schedulers["schedulers"]) == 1:
+        for key, value in param_schedulers["schedulers"][0].items():
+            param_schedulers[key] = value
+    return param_schedulers
+
+
+def get_scaled_lr_scheduler(cfg, param_schedulers, scaled_lr):
+    if param_schedulers["name"] == "cosine":
+        start_value = param_schedulers["start_value"]
+        end_value = param_schedulers["end_value"]
+        decay_multiplier = end_value / start_value
+        param_schedulers["start_value"] = float(scaled_lr)
+        param_schedulers["end_value"] = float(scaled_lr * decay_multiplier)
+    elif param_schedulers["name"] == "multistep" or param_schedulers["name"] == "step":
+        values = param_schedulers["values"]
+        gamma = 1.0
+        if len(values) > 1:
+            gamma = round(values[1] / values[0], 6)
+        new_values = []
+        for idx in range(len(values)):
+            new_values.append(round(float(scaled_lr * pow(gamma, idx)), 8))
+        param_schedulers["values"] = new_values
+    elif param_schedulers["name"] == "step_with_fixed_gamma":
+        param_schedulers["base_value"] = scaled_lr
+    elif param_schedulers["name"] == "composite":
+        has_linear_warmup = False
+        for idx in range(len(param_schedulers["schedulers"])):
+            if param_schedulers["schedulers"][idx]["name"] == "linear":
+                has_linear_warmup = True
+            scheduler = get_scaled_lr_scheduler(
+                cfg, param_schedulers["schedulers"][idx], scaled_lr
+            )
+            param_schedulers["schedulers"][idx] = scheduler
+        # in case of composite LR schedule, if there's linear warmup specified,
+        # we check if the warmup is meaningful or not. If not, we simplify the
+        # schedule.
+        if has_linear_warmup:
+            resolve_linear_schedule(cfg, param_schedulers)
+    elif param_schedulers["name"] == "linear":
+        param_schedulers["end_value"] = scaled_lr
+    elif param_schedulers["name"] == "constant":
+        param_schedulers["value"] = scaled_lr
+    else:
+        raise RuntimeError(
+            f"Unknow param_scheduler: {param_schedulers['name']}. NOT scaling linearly"
+        )
+    return param_schedulers
+
+
 def assert_hydra_conf(cfg):
     # some inference for the Info NCE loss.
     if "simclr_info_nce_loss" in cfg.CRITERION.name:
@@ -121,3 +198,18 @@ def assert_hydra_conf(cfg):
         queue_length -= queue_length % batch_size
         cfg.CRITERION.SWAV_LOSS.QUEUE.QUEUE_LENGTH = queue_length
         cfg.CRITERION.SWAV_LOSS.QUEUE.LOCAL_QUEUE_LENGTH = queue_length // world_size
+
+    # assert the Learning rate here. LR is scaled as per https://arxiv.org/abs/1706.02677.
+    # to turn this automatic scaling off,
+    # set config.OPTIMIZER.param_schedulers.lr.auto_lr_scaling.auto_scale=false
+    if cfg.OPTIMIZER.param_schedulers.lr.auto_lr_scaling.auto_scale:
+        world_size = cfg.DISTRIBUTED.NUM_NODES * cfg.DISTRIBUTED.NUM_PROC_PER_NODE
+        batch_size = cfg.DATA.TRAIN.BATCHSIZE_PER_REPLICA * world_size
+        param_schedulers = cfg.OPTIMIZER.param_schedulers.lr
+        base_lr = param_schedulers.auto_lr_scaling.base_value
+        base_lr_batch_size = param_schedulers.auto_lr_scaling.base_lr_batch_size
+        scale_factor = float(batch_size) / base_lr_batch_size
+        scaled_lr = base_lr * scale_factor
+        cfg.OPTIMIZER.param_schedulers.lr = get_scaled_lr_scheduler(
+            cfg, param_schedulers, scaled_lr
+        )
