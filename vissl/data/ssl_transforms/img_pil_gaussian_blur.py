Speedup swav training, fix contbuild and scaling on IG1B RNXT32x8d
Summary:
- moved IG dataset configs to fb/ config folder
- added a low-resource config for fbcode that can run any config on 1 gpu. helpful for debugging
- some improvements to low-resource configs (use pytorch for syncbn as apex might not be available)
- re-write gaussian blur using PIL that speeds up the training by 2x and SwAV matches SimCLR training speed
- added config files for swav training for IG1B - kicking off scaling work
- fix contbuild for object_detection_benchmark
- remove the ssn transforms , we will directly use them from the ssn codebase if needed.

Reviewed By: blefaudeux

Differential Revision: D22441349

fbshipit-source-id: aaee7a8c00a36966eecb13a414102e97f72907fd

vissl/data/ssl_transforms/img_pil_gaussian_blur.py
@@ -1,25 +1,24 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+import random
 from typing import Any, Dict
 
-import cv2
 import numpy as np
 from classy_vision.dataset.transforms import register_transform
 from classy_vision.dataset.transforms.classy_transform import ClassyTransform
-from PIL import Image
+from PIL import ImageFilter
 
 
 @register_transform("ImgPilGaussianBlur")
 class ImgPilGaussianBlur(ClassyTransform):
     """
-    Apply Gaussian Blur to the PIL image. Take the radius, kernel size and
-    probability of application as the parameter.
+    Apply Gaussian Blur to the PIL image. Take the radius and probability of
+    application as the parameter.
 
     This transform was used in SimCLR - https://arxiv.org/abs/2002.05709
     """
 
-    def __init__(self, kernel, p, radius_min, radius_max):
-        self.kernel = kernel
+    def __init__(self, p, radius_min, radius_max):
         self.prob = p
         self.radius_min = radius_min
         self.radius_max = radius_max
@@ -28,15 +27,15 @@ class ImgPilGaussianBlur(ClassyTransform):
         should_blur = np.random.rand() > self.prob
         if not should_blur:
             return img
+
         # randomly sample sigma
-        sigma = np.random.rand() * (self.radius_max - self.radius_min) + self.radius_min
-        blurred = cv2.GaussianBlur(np.asarray(img), (self.kernel, self.kernel), sigma)
-        return Image.fromarray(blurred)
+        sigma = random.uniform(self.radius_min, self.radius_max)
+        img = img.filter(ImageFilter.GaussianBlur(radius=sigma))
+        return img
 
     @classmethod
     def from_config(cls, config: Dict[str, Any]) -> "ImgPilGaussianBlur":
-        kernel = config.get("kernel", 23)
         prob = config.get("p", 0.5)
         radius_min = config.get("radius_min", 0.1)
         radius_max = config.get("radius_max", 2.0)
-        return cls(kernel=kernel, p=prob, radius_min=radius_min, radius_max=radius_max)
+        return cls(p=prob, radius_min=radius_min, radius_max=radius_max)