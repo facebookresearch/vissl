131d0c685b925e3d60c322ce3f6ace7fae9c6253
Priya Goyal <prigoyal@fb.com>
2020-07-20 08:16:49 -0700

    Change default quick model to 32x8d and more models in model zoo
    Summary: we decided on 32x8d as default architecture for trianing. adding configs for scales/crops ablations. updating model zoo with model checkpoints
    
    Differential Revision: D22598162
    
    fbshipit-source-id: 1391a7ed6cd0d1b307b141907098a7cb49778496

--- vissl/ssl_optimizers/__init__.py
+++ vissl/ssl_optimizers/__init__.py
@@ -1,7 +1,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
 
-from vissl.ssl_optimizers.optimizer_helper import get_optimizer_regularized_params
+from vissl.ssl_optimizers.optimizer_helper import (  # noqa
+    get_optimizer_regularized_params,
+)
 
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
--- vissl/ssl_trainer/__init__.py
+++ vissl/ssl_trainer/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-from vissl.ssl_trainer.trainer import DistributedSelfSupervisionTrainer
+from vissl.ssl_trainer.trainer import DistributedSelfSupervisionTrainer  # noqa
 
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
