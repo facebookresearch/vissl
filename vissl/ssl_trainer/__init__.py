f44bbbb37774534ee6ca3c8927239eecbb552bce
Priya Goyal <prigoyal@fb.com>
2020-08-10 06:02:27 -0700

    Make trainer as the main entry point and refactor task, trainer, steps logic
    Differential Revision: D22878016
    
    fbshipit-source-id: 299c12924f4a12cd0f13c30f73af023c2f0a8dfb

--- vissl/ssl_trainer/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
-from vissl.ssl_trainer.trainer import DistributedSelfSupervisionTrainer  # noqa
-
-
-__all__ = [k for k in globals().keys() if not k.startswith("_")]
