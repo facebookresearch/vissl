aaf85c5b4dccbd7dae4ee5530bb356acbf8c5d45
QuentinDuval <QuentinDuval@users.noreply.github.com>
2021-01-15 15:24:35 -0500

    Rework the script which runs jobs on SLURM to be in Python and use the content of the configuration of an experiment to automatically select the right amount of nodes and GPUs

---
+++ dev/launch_slurm_2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+######################### INPUT PARAMS ##################################
+
+EXPT_NAME=${EXPT_NAME-'unnamed'}
+PARTITION=${PARTITION-learnfair}
+COMMENT=${COMMENT-ggl4i_gcd}
+BRANCH=${BRANCH-master}
+RUN_ID=$(date +'%Y%m%d')
+CFG=( "$@" )
+
+####################### setup experiment dir ###################################
+
+# create the experiments folder
+EXP_ROOT_DIR="/checkpoint/$USER/vissl/${RUN_ID}/$EXPT_NAME/"
+RUN_SCRIPT="$EXP_ROOT_DIR/tools/run_distributed_on_slurm.py"
+CHECKPOINT_DIR="$EXP_ROOT_DIR/checkpoints/"
+
+echo "EXP_ROOT_DIR: $EXP_ROOT_DIR"
+echo "RUN_SCRIPT: $RUN_SCRIPT"
+echo "CHECKPOINT_DIR: $CHECKPOINT_DIR"
+
+# make the exp_dir and clone the current code inside it
+rm -rf $EXP_ROOT_DIR
+mkdir -p "$EXP_ROOT_DIR"
+mkdir -p "$CHECKPOINT_DIR"
+cp -r . $EXP_ROOT_DIR
+
+####################### launch script #########################################
+
+python -u "$RUN_SCRIPT" "${CFG[*]}" \
+  hydra.run.dir="$EXP_ROOT_DIR"\
+  +name="$EXPT_NAME"  \
+  +comment="$COMMENT"  \
+  +partition="$PARTITION"  \
+  +log_folder="$EXP_ROOT_DIR"  \
+  config.CHECKPOINT.DIR="$CHECKPOINT_DIR"
---
+++ tools/run_distributed_on_slurm.py
@@ -0,0 +1,35 @@
+import sys
+from typing import List, Any
+
+from hydra.experimental import initialize_config_module, compose
+
+from vissl.utils.distributed_launcher import schedule_on_slurm
+from vissl.utils.hydra_config import is_hydra_available, convert_to_attrdict
+
+
+def hydra_main(overrides: List[Any]):
+    print(f"####### overrides: {overrides}")
+    with initialize_config_module(config_module="vissl.config"):
+        cfg = compose("defaults", overrides=overrides)
+
+    args, config = convert_to_attrdict(cfg)
+    config.DATA.NUM_DATALOADER_WORKERS = 8
+    schedule_on_slurm(
+        engine_name=args.engine_name,
+        config=config,
+        job_name=args.name,
+        job_comment=args.comment,
+        partition=args.partition,
+        log_folder=args.log_folder)
+
+
+if __name__ == "__main__":
+    """
+    Example usage:
+
+    `python tools/run_distributed_on_slurm.py config=test/integration_test/quick_simclr`
+    """
+    overrides = sys.argv[1:]
+    assert is_hydra_available(), "Make sure to install hydra"
+    overrides.append("hydra.verbose=true")
+    hydra_main(overrides=overrides)
