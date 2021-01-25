b3b1a613c3c8005391cba155ee827073195e34d0
Jeremy Francis Reizenstein <bottler@users.noreply.github.com>
2021-01-25 14:31:12 -0800

    remove unneeded recipe

--- dev/packaging/classy_conda/build_conda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-set -ex
-
-mkdir -p "./out"
-
-conda build -c pytorch -c conda-forge -c defaults --no-anaconda-upload --python "$PYTHON_VERSION" --output-folder "./out" "./ClassyVision"
