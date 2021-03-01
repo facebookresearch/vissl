4f091a946b52486cc309bcc59486525d1a72fcc7
Priya Goyal <prigoyal@fb.com>
2021-03-01 08:27:49 -0800

    Make opencv an optional dependency in VISSL
    Differential Revision: D26724383
    
    fbshipit-source-id: 032c95c55bb8cd0eac5296aaddd7301386936de0

--- dev/packaging/vissl_pip/test/test.sh
+++ dev/packaging/vissl_pip/test/test.sh
@@ -14,7 +14,7 @@ conda install -y -c pytorch pytorch=1.5.1 cudatoolkit=10.1 torchvision
 pip install apex -f https://dl.fbaipublicfiles.com/vissl/packaging/apexwheels/py37_cu101_pyt151/download.html
 #pip install vissl --no-index -f https://dl.fbaipublicfiles.com/vissl/packaging/visslwheels/download.html
 pip install vissl
-python -c "import vissl, apex, cv2"
+python -c "import vissl, apex"
 cd loc1
 python -m unittest discover -v -s tests
 dev/run_quick_tests.sh
