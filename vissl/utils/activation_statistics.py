54c45d6a881bca459522e6d0eb688e441259a09b
QuentinDuval <QuentinDuval@users.noreply.github.com>
2021-04-06 08:31:45 -0400

    [feature] (activation stats): ability to track the statistics of each layer in the model when training - reset the _prev_module_name in post_forward_hook (avoid potential future bugs)

--- vissl/utils/activation_statistics.py
+++ vissl/utils/activation_statistics.py
@@ -108,8 +108,11 @@ class ActivationStatisticsMonitor:
 
     def _create_post_forward_hook(self, name: str):
         def _post_forward_hook(module: nn.Module, inputs, outputs):
+
             # Eliminate non-leaf modules as well as modules ignored by the forward
-            if self._prev_module_name != name:
+            previous_forward_module_name = self._prev_module_name
+            self._prev_module_name = None
+            if previous_forward_module_name != name:
                 return
 
             # Collect all outputs
