3b33a1c8c1d0b7a6fb51dfee02045086529ebd73
QuentinDuval <QuentinDuval@users.noreply.github.com>
2021-05-12 08:48:36 -0700

    VISSL Profiling: add autograd profiler, new kineto profiler and nvtx in VISSL (#139)
    Summary:
    # Profiling in VISSL
    
    Plugging in VISSL the necessary tooling to do profiling with:
    - the legacy `torch.autograd.profiler` profiler
    - the new `torch.profiler` in pytorch 1.8.1
    - nvtx (nsys + nsys-ui)
    
    - [x] Try the `torch.autograd.profiler`: it works but is not able to see the AllGather (no details on the CUDA streams)
    - [x] Try the `torch.profiler` (pytorch 1.8.1): it can see the full CUDA stream details, link it to pytorch operations, and in particular see the AllGather kernels
    - [x] Try nsys on test script of 10B model: shows great details for CUDA streams but no link to the pytorch operations
    - [x] Plug the schedule for `torch.profiler` and an emulation of of it for `torch.autograd.profiler`
    - [x] Enrich the configuration to select which part to profile (CUDA or CPU or both)
    
    **Conclusion**: the best profiling tool for now is the new `torch.profiler` as it provides the most comprehensive view of what happens during the training. All options are nevertheless provided to suit all use cases.
    
    ## How to use the feature
    
    ### New Pytorch profiler
    
    Add the following options to profile between iteration 5 and 8, with a warmup of 2 iterations prior to the collection of traces:
    
    ```
    config.PROFILING.START_ITERATION=3 config.PROFILING.WARMUP_ITERATIONS=2 config.PROFILING.NUM_ITERATIONS=3 config.PROFILING.INTERRUPT_TRAINING=True config.PROFILING.RUNTIME_PROFILING.USE_PROFILER=True
    ```
    
    Example of collected traces (with CUDA stream full details):
    
    <img width="1163" alt="Screenshot 2021-05-10 at 11 13 46" src="https://user-images.githubusercontent.com/7412790/117682197-d1e04c00-b180-11eb-8922-c0f5ebdc82ea.png">
    
    ### Autograd profiler (pytorch < 1.8.1)
    
    Add the following options to profile between iteration 5 and 8 (the warmup is only used to delay the collection of traces here):
    
    ```
    config.PROFILING.START_ITERATION=3 config.PROFILING.WARMUP_ITERATIONS=2 config.PROFILING.NUM_ITERATIONS=3 config.PROFILING.INTERRUPT_TRAINING=True config.PROFILING.RUNTIME_PROFILING.USE_PROFILER=True
    ```
    
    Example of collected traces (we don't see the CUDA streams):
    
    <img width="1169" alt="Screenshot 2021-05-10 at 11 14 55" src="https://user-images.githubusercontent.com/7412790/117682357-f9371900-b180-11eb-8bea-c7a6fcf2e97d.png">
    
    ### NSYS (NVTX)
    
    Use `nsys profile -o profile_output python` followed by the python command to run. You can then use `nsys-gui` to analyse visually the traces. This way of profiling is not integrated with SLURM `submitit`.
    
    Example of traces:
    
    <img width="878" alt="Screenshot 2021-05-06 at 18 17 34" src="https://user-images.githubusercontent.com/7412790/117372365-630ca580-ae97-11eb-8264-c5d552ba0345.png">
    
    Another part of the trace:
    
    <img width="1265" alt="Screenshot 2021-05-06 at 18 19 30" src="https://user-images.githubusercontent.com/7412790/117372508-a404ba00-ae97-11eb-84ff-3516ba95d010.png">
    
    With this tool, we can see the different streams and what kernel executes on each.
    
    ## Interested parties
    
    CC: prigoyal min-xu-ai
    
    Pull Request resolved: https://github.com/fairinternal/ssl_scaling/pull/139
    
    Reviewed By: min-xu-ai, prigoyal
    
    Differential Revision: D28302033
    
    Pulled By: QuentinDuval
    
    fbshipit-source-id: c7e8a465e4f633b2d9af0c50772390f386ede6cc

--- vissl/hooks/profiling_hook.py
+++ vissl/hooks/profiling_hook.py
@@ -11,6 +11,7 @@ from classy_vision.hooks.classy_hook import ClassyHook
 from vissl.config.attr_dict import AttrDict
 from vissl.utils.env import get_machine_local_and_dist_rank
 from vissl.utils.layer_memory_tracking import LayerwiseMemoryTracker
+from vissl.utils.profiler import create_runtime_profiler
 
 
 class ProfilingHook(ClassyHook):
@@ -31,21 +32,42 @@ class ProfilingHook(ClassyHook):
         Returns whether or not the profiler hook should be instantiated:
         it should be enabled if any of the profiling options is on
         """
-        return profiling_config.MEMORY_PROFILING.TRACK_BY_LAYER_MEMORY
+        with_runtime_profiling = profiling_config.RUNTIME_PROFILING.USE_PROFILER and (
+            profiling_config.RUNTIME_PROFILING.PROFILE_CPU
+            or profiling_config.RUNTIME_PROFILING.PROFILE_GPU
+        )
+        return (
+            profiling_config.MEMORY_PROFILING.TRACK_BY_LAYER_MEMORY
+            or with_runtime_profiling
+        )
 
     def __init__(self, profiling_config: AttrDict):
         super().__init__()
         self.output_folder = profiling_config.OUTPUT_FOLDER
-        self.start_iteration = profiling_config.START_ITERATION
-        self.end_iteration = (
-            profiling_config.START_ITERATION + profiling_config.NUM_ITERATIONS
+        self.start_iteration = (
+            profiling_config.START_ITERATION + profiling_config.WARMUP_ITERATIONS
         )
+        self.end_iteration = self.start_iteration + profiling_config.NUM_ITERATIONS
+        self.interrupt_training = profiling_config.STOP_TRAINING_AFTER_PROFILING
         self.dist_rank = get_machine_local_and_dist_rank()[1]
-        self.enabled = self.dist_rank in profiling_config.PROFILED_RANKS
-        self.profile_memory = (
-            self.enabled and profiling_config.MEMORY_PROFILING.TRACK_BY_LAYER_MEMORY
+        self.is_profiling_rank = self.dist_rank in profiling_config.PROFILED_RANKS
+        self.profile_runtime = (
+            self.is_profiling_rank and profiling_config.RUNTIME_PROFILING.USE_PROFILER
         )
-        if self.profile_memory:
+        self.runtime_profiler = create_runtime_profiler(
+            enabled=self.profile_runtime,
+            use_cpu=profiling_config.RUNTIME_PROFILING.PROFILE_CPU,
+            use_cuda=profiling_config.RUNTIME_PROFILING.PROFILE_GPU,
+            wait=profiling_config.START_ITERATION,
+            warmup=profiling_config.WARMUP_ITERATIONS,
+            active=profiling_config.NUM_ITERATIONS,
+            legacy_profiler=profiling_config.RUNTIME_PROFILING.LEGACY_PROFILER,
+        )
+        self.profile_by_layer_memory = (
+            self.is_profiling_rank
+            and profiling_config.MEMORY_PROFILING.TRACK_BY_LAYER_MEMORY
+        )
+        if self.profile_by_layer_memory:
             logging.info(f"Setting up memory tracker for rank {self.dist_rank}...")
             self.layer_memory_tracker = LayerwiseMemoryTracker()
 
@@ -53,32 +75,55 @@ class ProfilingHook(ClassyHook):
         """
         Called at the start of training.
         """
-        if self.profile_memory:
+        if self.profile_by_layer_memory:
             assert (
                 task.use_gpu is True
             ), "Profiling memory usage requires training on GPU"
-        if self.profile_memory and self.start_iteration == 0:
+        if self.profile_by_layer_memory and self.start_iteration == 0:
             self.layer_memory_tracker.monitor(task.base_model)
+        if self.profile_runtime:
+            self.runtime_profiler.__enter__()
 
     def on_exception(self, task: "tasks.ClassyTask"):
-        if self.profile_memory:
+        if self.profile_by_layer_memory:
             iteration = task.local_iteration_num
             self._dump_memory_stats(iteration)
+        if self.profile_runtime:
+            self._dump_runtime_profiling_results(stop_profiler=True)
 
     def on_end(self, task: "tasks.ClassyTask") -> None:
         """
         Called at the end of training.
         """
-        if self.profile_memory:
+        if self.profile_by_layer_memory:
             self.layer_memory_tracker.stop()
+        if self.profile_runtime:
+            self._dump_runtime_profiling_results(stop_profiler=True)
 
     def on_update(self, task: "tasks.ClassyTask") -> None:
         """
         Called after parameter update.
         """
-        if self.profile_memory:
-            iteration = task.local_iteration_num
+        iteration = task.local_iteration_num
+        next_iteration = iteration + 1
+
+        if self.profile_by_layer_memory:
             self._memory_tracking(iteration, task)
+        if self.profile_runtime:
+            self._runtime_tracking(iteration, task)
+        if self.interrupt_training and next_iteration >= self.end_iteration + 1:
+            self._interrupt_training(iteration)
+
+    def _runtime_tracking(self, iteration: int, task: "tasks.ClassyTask"):
+        """
+        Handle the runtime profiling logic:
+        - enabling / disabling the tracker depending on the iteration
+        - dumping the statistics when profiling ends
+        """
+        next_iteration = iteration + 1
+        self.runtime_profiler.step()
+        if next_iteration == self.end_iteration:
+            self._dump_runtime_profiling_results(stop_profiler=True)
 
     def _memory_tracking(self, iteration: int, task: "tasks.ClassyTask"):
         """
@@ -101,9 +146,19 @@ class ProfilingHook(ClassyHook):
         if next_iteration == self.end_iteration:
             self.layer_memory_tracker.stop()
 
+    def _interrupt_training(self, iteration: int):
+        raise Exception("End of profiling: shutting down...")
+
     def _dump_memory_stats(self, iteration: int):
         image = self.layer_memory_tracker.show_plots(capture=True)
         image_name = f"memory_rank_{self.dist_rank}_iteration_{iteration}.jpg"
         image.save(os.path.join(self.output_folder, image_name))
         json_name = f"memory_rank_{self.dist_rank}_iteration_{iteration}.json"
-        self.layer_memory_tracker.save_traces(json_name)
+        self.layer_memory_tracker.save_traces(
+            os.path.join(self.output_folder, json_name)
+        )
+
+    def _dump_runtime_profiling_results(self, stop_profiler: bool = False):
+        if stop_profiler:
+            self.runtime_profiler.__exit__(None, None, None)
+        self.runtime_profiler.dump(folder=self.output_folder, rank=self.dist_rank)
