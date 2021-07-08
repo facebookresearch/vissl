9d961cdaabb3b561ce3df9903712f62e6f9d176d
Quentin Duval <qduval@fb.com>
2021-07-08 11:49:24 -0700

    Rework the tests to make sure that all GPU tests are ran and that skipped tests do not trigger warnings upon integration (#356)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/vissl/pull/356
    
    Two main changes:
    - Make sure that all GPU tests are run on CircleCI
    - Make sure that skipped GPU tests do not trigger warnings
    
    This required to split the test_losses in part and update some other tests as well.
    
    Reviewed By: prigoyal
    
    Differential Revision: D29557107
    
    fbshipit-source-id: a46823097983d0b58bc6d20bab5fef03820dd07d

--- vissl/utils/test_utils.py
+++ vissl/utils/test_utils.py
@@ -3,12 +3,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import functools
 import os
 import re
 import tempfile
 from contextlib import contextmanager
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import Callable, List, Tuple
 
 import torch
 import torch.distributed as dist
@@ -76,10 +77,16 @@ def gpu_test(gpu_count: int = 1):
     Annotation for GPU tests, skipping the test if the
     required amount of GPU is not available
     """
-    import unittest
 
-    message = f"Not enough GPUs to run the test: required {gpu_count}"
-    return unittest.skipIf(torch.cuda.device_count() < gpu_count, message)
+    def gpu_test_decorator(test_function: Callable):
+        @functools.wraps(test_function)
+        def wrapped_test(*args, **kwargs):
+            if torch.cuda.device_count() >= gpu_count:
+                return test_function(*args, **kwargs)
+
+        return wrapped_test
+
+    return gpu_test_decorator
 
 
 def init_distributed_on_file(world_size: int, gpu_id: int, sync_file: str):
