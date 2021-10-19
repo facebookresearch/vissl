9be692c4ce2f9abc2331d73d97bf1d0461c53adc
Mayalen Etcheverry <etcheverry.mayalen@gmail.com>
2021-10-19 11:44:34 -0700

    add option to register custom models (#416)
    Summary:
    This pull request is related to issue https://github.com/facebookresearch/vissl/issues/411
    
    Pull Request resolved: https://github.com/facebookresearch/vissl/pull/416
    
    Reviewed By: prigoyal, QuentinDuval
    
    Differential Revision: D31608966
    
    Pulled By: iseessel
    
    fbshipit-source-id: 221c4d4ee9c08484917b564a885036d2546b698f

--- vissl/models/__init__.py
+++ vissl/models/__init__.py
@@ -3,11 +3,61 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from vissl.models.base_ssl_model import BaseSSLMultiInputOutputModel
+"""
+Here we create all the models required for SSL. The default model is
+BaseSSLMultiInputOutputModel, however users can create their own model.
+See #register_model below.
+"""
+
+from pathlib import Path
+
+from classy_vision.generic.registry_utils import import_all_modules
 from vissl.models.model_helpers import (  # noqa
     convert_sync_bn,
     is_feature_extractor_model,
 )
+
+
+MODEL_REGISTRY = {}
+MODEL_NAMES = set()
+
+
+def register_model(name):
+    """
+    Registers Self-Supervision Model.
+
+    This decorator allows VISSL to add custom models, even if the
+    model itself is not part of VISSL. To use it, apply this decorator
+    to a model class, like this:
+
+    .. code-block:: python
+
+        @register_model('my_model_name')
+        class MyModelName():
+            ...
+
+    To get a model from a configuration file, see :func:`get_model`. The default
+    model is BaseSSLMultiInputOutputModel.
+    """
+
+    def register_model_class(func):
+        if name in MODEL_REGISTRY:
+            raise ValueError("Cannot register duplicate model ({})".format(name))
+
+        MODEL_REGISTRY[name] = func
+        MODEL_NAMES.add(func.__name__)
+        return func
+
+    return register_model_class
+
+
+def get_model(model_name: str):
+    """
+    Lookup the model_name in the model registry and return.
+    If the model is not implemented, asserts will be thrown and workflow will exit.
+    """
+    assert model_name in MODEL_REGISTRY, "Unknown model"
+    return MODEL_REGISTRY[model_name]
 
 
 def build_model(model_config, optimizer_config):
@@ -16,7 +66,11 @@ def build_model(model_config, optimizer_config):
     The returned model is not copied to gpu yet (if using gpu) and neither
     wrapped with DDP yet. This is done later train_task.py .prepare()
     """
-    return BaseSSLMultiInputOutputModel(model_config, optimizer_config)
+    model_name = model_config.BASE_MODEL_NAME
+    model_cls = get_model(model_name)
+    return model_cls(model_config, optimizer_config)
 
 
-__all__ = [k for k in globals().keys() if not k.startswith("_")]
+# automatically import any Python files in the models/ directory
+FILE_ROOT = Path(__file__).parent
+import_all_modules(FILE_ROOT, "vissl.models")
