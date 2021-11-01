47c1ad586f61eab2b41a49101219e0a0aeef232f
Isaac Seessel <iseessel@fb.com>
2021-11-01 13:38:25 -0400

    Add fairscale directly into package binary

--- dev/packaging/vissl_pip/go.sh
+++ dev/packaging/vissl_pip/go.sh
@@ -9,9 +9,15 @@ git clone https://github.com/facebookresearch/ClassyVision.git
 cd ClassyVision || exit
 git reset --hard 4785d5ee19d3bcedd5b28c1eb51ea1f59188b54d
 cd ../ || exit
-
 rm -rf ../../../classy_vision
 cp -r ClassyVision/classy_vision ../../../classy_vision
-rm -rf ../../../fairscale
 
-docker run --rm  -v "$PWD/../../..:/inside pytorch/conda-cuda bash inside/dev/packaging/vissl_pip/inside.sh"
+rm -rf fairscale
+git clone https://github.com/facebookresearch/fairscale.git
+cd fairscale || exit
+git reset --hard df7db85cef7f9c30a5b821007754b96eb1f977b6
+cd ../ || exit
+rm -rf ../../../fairscale
+cp -r fairscale/fairscale ../../../fairscale
+
+docker run --rm  -v $PWD/../../..:/inside pytorch/conda-cuda bash inside/dev/packaging/vissl_pip/inside.sh
