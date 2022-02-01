9480070b45bc7bcc981dbb12eef78e1bc76ab8e1
Priya Goyal <prigoyal@fb.com>
2022-02-01 05:49:40 -0800

    Apply code changes only
    Differential Revision: D33791248
    
    fbshipit-source-id: a63f2dfdba714cd98031b677e026a07eb2ac412c

--- extra_scripts/convert_folder_to_filelist.py
+++ extra_scripts/convert_folder_to_filelist.py
@@ -4,9 +4,10 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-This script creates .npy filelist for all splits in the input path.
+- This script creates .npy filelist for all splits in the input path.
 The script assumes that the input directory has the structure of
 the torchvision ImageFolder.
+- The script also works if the data doesn't have any splits.
 
 Example usage:
 
@@ -23,7 +24,49 @@ import os
 
 from iopath.common.file_io import g_pathmgr
 from vissl.utils.env import setup_path_manager
-from vissl.utils.io import save_file, cleanup_dir
+from vissl.utils.io import save_file
+
+
+def get_filelist_labels_images_paths(input_path):
+    dataset_split_summary = {}
+    img_paths, img_labels = [], []
+    label_paths = g_pathmgr.ls(input_path)
+    dataset_split_summary["labels"] = label_paths
+    dataset_split_summary["num_labels"] = len(label_paths)
+    print(f"{len(label_paths)} classes found.")
+
+    total_split_examples = 0
+    # Populate the img_paths and img_labels based on torchvision image folder file structure.
+    for label in label_paths:
+        label_path = os.path.join(input_path, label)
+        images = g_pathmgr.ls(os.path.join(input_path, label))
+        print(f"{len(images)} examples found for {label}.")
+        total_split_examples += len(images)
+        for image in images:
+            img_path = os.path.join(label_path, image)
+            img_paths.append(img_path)
+            img_labels.append(label)
+
+    # print the dataset summary
+    dataset_split_summary["num_examples"] = total_split_examples
+    print(f"{total_split_examples} found")
+    return dataset_split_summary, img_paths, img_labels
+
+
+def save_img_labels_filelist(
+    img_paths, img_labels, out_image_filepath, out_label_filepath
+):
+    # Remove the split .npy filelist if they exist and resave them.
+    if g_pathmgr.exists(out_image_filepath):
+        g_pathmgr.rm(out_image_filepath)
+    save_file(img_paths, out_image_filepath)
+    print(f"Saved: {out_image_filepath}")
+
+    if g_pathmgr.exists(out_label_filepath):
+        g_pathmgr.rm(out_label_filepath)
+    save_file(img_labels, out_label_filepath)
+    print(f"Saved: {out_label_filepath}")
+    print("Saved!!")
 
 
 def get_argument_parser():
@@ -50,7 +93,7 @@ if __name__ == "__main__":
     """
     Example usage:
 
-    buck-out/gen/deeplearning/projects/ssl_framework/extra_scripts/fb/convert_folder_to_filelist.par \  # NOQA
+    python extra_scripts/convert_folder_to_filelist.par \
         -i "manifold://ssl_framework/tree/datasets/food_101/" \
         -o "manifold://ssl_framework/tree/datasets/food_101/"
     """
@@ -58,53 +101,45 @@ if __name__ == "__main__":
 
     setup_path_manager()
 
-    splits = g_pathmgr.ls(args.input)
-    print(f"The following splits are found: { ','.join(splits) }")
+    ground_truth_splits = ["train", "trainval", "val", "test"]
+    available_splits = g_pathmgr.ls(args.input)
 
     dataset_summary = {}
 
-    for split in ["train", "trainval", "val", "test"]:
-        if not g_pathmgr.exists(os.path.join(args.input, split)):
-            continue
+    if not any(split in available_splits for split in ground_truth_splits):
+        # the dataset doesn't have any splits. So we just read it as is
+        print("Dataset has no splits...")
+        dataset_summary, img_paths, img_labels = get_filelist_labels_images_paths(
+            args.input
+        )
+        out_image_filepath = os.path.join(args.output, "images.npy")
+        out_label_filepath = os.path.join(args.output, "labels.npy")
+        save_img_labels_filelist(
+            img_paths, img_labels, out_image_filepath, out_label_filepath
+        )
+    else:
+        for split in ["train", "trainval", "val", "test"]:
+            if not g_pathmgr.exists(os.path.join(args.input, split)):
+                continue
 
-        dataset_summary[split] = {}
-        img_paths = []
-        img_labels = []
+            dataset_summary[split] = {}
+            split_path = os.path.join(args.input, split)
+            print(f"Getting data filelist for split: {split}")
+            (
+                dataset_summary[split],
+                img_paths,
+                img_labels,
+            ) = get_filelist_labels_images_paths(split_path)
 
-        split_path = os.path.join(args.input, split)
-        label_paths = g_pathmgr.ls(split_path)
-        dataset_summary[split]["labels"] = label_paths
-        dataset_summary[split]["num_labels"] = len(label_paths)
-        print(f"{len(label_paths)} classes found for { split } split.")
-
-        total_split_examples = 0
-        # Populate the img_paths and img_labels based on torchvision image folder file structure.
-        for label in label_paths:
-            label_path = os.path.join(split_path, label)
-            images = g_pathmgr.ls(os.path.join(split_path, label))
-            print(f"{len(images)} examples found for { label }, { split }.")
-            total_split_examples += len(images)
-            for image in images:
-                img_path = os.path.join(label_path, image)
-                img_paths.append(img_path)
-                img_labels.append(label)
-
-        dataset_summary[split]["num_examples"] = total_split_examples
-        print(f"{ total_split_examples } found for { split } split \n")
-        # Remove the split .npy filelist if they exist and resave them..
-        image_path = os.path.join(args.output, f"{split}_images.npy")
-
-        cleanup_dir(image_path)
-        save_file(img_paths, image_path)
-        print(f"Saved { image_path }")
-
-        label_path = os.path.join(args.output, f"{split}_labels.npy")
-
-        cleanup_dir(label_path)
-        save_file(img_labels, label_path)
-        print(f"Saved { label_path }")
+            # Remove the split .npy filelist if they exist and resave them..
+            out_image_filepath = os.path.join(args.output, f"{split}_images.npy")
+            out_label_filepath = os.path.join(args.output, f"{split}_labels.npy")
+            save_img_labels_filelist(
+                img_paths, img_labels, out_image_filepath, out_label_filepath
+            )
 
     # Save dataset summary.
     dataset_summary_path = os.path.join(args.output, "dataset_summary.json")
-    cleanup_dir(dataset_summary_path)
+    if g_pathmgr.exists(dataset_summary_path):
+        g_pathmgr.rm(dataset_summary_path)
     save_file(dataset_summary, dataset_summary_path)
