6f70b7073f94e73e389863b0465d7edd1e2d839c
QuentinDuval <QuentinDuval@users.noreply.github.com>
2022-03-04 12:33:32 -0800

    test script to display the result of configurations composition (#226)
    Summary: Pull Request resolved: https://github.com/fairinternal/ssl_scaling/pull/226
    
    Reviewed By: iseessel
    
    Differential Revision: D34643165
    
    Pulled By: QuentinDuval
    
    fbshipit-source-id: 26c3c21a83f2eda03d02dedfffca0d1b7d0b1b1f

---
+++ extra_scripts/debugging/test_config.py
@@ -0,0 +1,20 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+import pprint
+import sys
+from typing import List, Any
+
+from vissl.utils.hydra_config import compose_hydra_configuration, convert_to_attrdict
+
+
+def hydra_main(overrides: List[Any]):
+    cfg = compose_hydra_configuration(overrides)
+    args, config = convert_to_attrdict(cfg)
+    print(pprint.pformat(config))
+
+
+if __name__ == "__main__":
+    overrides = sys.argv[1:]
+    hydra_main(overrides=overrides)
