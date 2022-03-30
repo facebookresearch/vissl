0a81fbad89827d81a6d95478c035e41bd9fc338a
QuentinDuval <QuentinDuval@users.noreply.github.com>
2022-03-30 05:56:39 -0700

    Refactor ViT to remove the hacky workaround (#244)
    Summary: Pull Request resolved: https://github.com/fairinternal/ssl_scaling/pull/244
    
    Reviewed By: mannatsingh
    
    Differential Revision: D35076373
    
    Pulled By: QuentinDuval
    
    fbshipit-source-id: 248f7daf831b0e8bdee1ee67febf44a727889996

--- vissl/models/trunks/vision_transformer.py
+++ vissl/models/trunks/vision_transformer.py
@@ -25,7 +25,6 @@ Leavitt (ito@fb.com, matthew.l.leavitt@gmail.com) and Vedanuj Goswami
 (vedanuj@fb.com).
 """
 
-import copy
 import logging
 import math
 from functools import partial
@@ -183,38 +182,37 @@ class VisionTransformer(nn.Module):
         super().__init__()
 
         assert model_config.INPUT_TYPE in ["rgb", "bgr"], "Input type not supported"
-        trunk_config = copy.deepcopy(model_config.TRUNK.VISION_TRANSFORMERS)
-
         logging.info("Building model: Vision Transformer from yaml config")
-        # Hacky workaround
-        trunk_config = AttrDict({k.lower(): v for k, v in trunk_config.items()})
 
-        img_size = trunk_config.image_size
-        patch_size = trunk_config.patch_size
-        in_chans = 3
-        embed_dim = trunk_config.hidden_dim
-        depth = trunk_config.num_layers
-        num_heads = trunk_config.num_heads
-        mlp_ratio = 4.0
-        qkv_bias = trunk_config.qkv_bias
-        qk_scale = trunk_config.qk_scale
-        drop_rate = trunk_config.dropout_rate
-        attn_drop_rate = trunk_config.attention_dropout_rate
-        drop_path_rate = trunk_config.drop_path_rate
-        hybrid_backbone_string = None
+        self.model_config = model_config
+        self.trunk_config = model_config.TRUNK.VISION_TRANSFORMERS
+        self.img_size = self.trunk_config.IMAGE_SIZE
+        self.patch_size = self.trunk_config.PATCH_SIZE
+        self.in_chans = 3
+        self.embed_dim = self.trunk_config.HIDDEN_DIM
+        self.depth = self.trunk_config.NUM_LAYERS
+        self.num_heads = self.trunk_config.NUM_HEADS
+        self.mlp_ratio = 4.0
+        self.qkv_bias = self.trunk_config.QKV_BIAS
+        self.qk_scale = self.trunk_config.QK_SCALE
+        self.drop_rate = self.trunk_config.DROPOUT_RATE
+        self.attn_drop_rate = self.trunk_config.ATTENTION_DROPOUT_RATE
+        self.drop_path_rate = self.trunk_config.DROP_PATH_RATE
+
         # TODO Implement hybrid backbones
-        if "HYBRID" in trunk_config.keys():
-            hybrid_backbone_string = trunk_config.HYBRID
+        hybrid_backbone_string = None
+        if "HYBRID" in self.trunk_config.keys():
+            hybrid_backbone_string = self.trunk_config.HYBRID
+
         norm_layer = partial(nn.LayerNorm, eps=1e-6)
 
-        self.num_features = (
-            self.embed_dim
-        ) = embed_dim  # num_features for consistency with other models
+        # num_features for consistency with other models
+        self.num_features = self.embed_dim
 
         # TODO : Enable Hybrid Backbones
         if hybrid_backbone_string:
             self.patch_embed = globals()[hybrid_backbone_string](
-                out_dim=embed_dim, img_size=img_size
+                out_dim=self.embed_dim, img_size=self.img_size
             )
         # if hybrid_backbone is not None:
         #     self.patch_embed = HybridEmbed(
@@ -225,37 +223,19 @@ class VisionTransformer(nn.Module):
         #     )
         else:
             self.patch_embed = PatchEmbed(
-                img_size=img_size,
-                patch_size=patch_size,
-                in_chans=in_chans,
-                embed_dim=embed_dim,
+                img_size=self.img_size,
+                patch_size=self.patch_size,
+                in_chans=self.in_chans,
+                embed_dim=self.embed_dim,
             )
         num_patches = self.patch_embed.num_patches
 
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
-        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
-        self.pos_drop = nn.Dropout(p=drop_rate)
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, self.embed_dim))
+        self.pos_drop = nn.Dropout(p=self.drop_rate)
 
-        dpr = [
-            x.item() for x in torch.linspace(0, drop_path_rate, depth)
-        ]  # stochastic depth decay rule
-        self.blocks = nn.ModuleList(
-            [
-                Block(
-                    dim=embed_dim,
-                    num_heads=num_heads,
-                    mlp_ratio=mlp_ratio,
-                    qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
-                    drop=drop_rate,
-                    attn_drop=attn_drop_rate,
-                    drop_path=dpr[i],
-                    norm_layer=norm_layer,
-                )
-                for i in range(depth)
-            ]
-        )
-        self.norm = norm_layer(embed_dim)
+        self.blocks = self._build_blocks(norm_layer)
+        self.norm = norm_layer(self.embed_dim)
 
         # NOTE as per official impl, we could have a pre-logits
         # representation dense layer + tanh here
@@ -265,6 +245,29 @@ class VisionTransformer(nn.Module):
         trunc_normal_(self.pos_embed, std=0.02)
         trunc_normal_(self.cls_token, std=0.02)
         self.apply(self._init_weights)
+
+    def _build_blocks(self, norm_layer) -> nn.ModuleList:
+        dpr = [
+            x.item() for x in torch.linspace(0, self.drop_path_rate, self.depth)
+        ]  # stochastic depth decay rule
+        blocks = []
+        for i in range(self.depth):
+            block = self._build_block(dpr[i], norm_layer)
+            blocks.append(block)
+        return nn.ModuleList(blocks)
+
+    def _build_block(self, dpr: float, norm_layer) -> nn.Module:
+        return Block(
+            dim=self.embed_dim,
+            num_heads=self.num_heads,
+            mlp_ratio=self.mlp_ratio,
+            qkv_bias=self.qkv_bias,
+            qk_scale=self.qk_scale,
+            drop=self.drop_rate,
+            attn_drop=self.attn_drop_rate,
+            drop_path=dpr,
+            norm_layer=norm_layer,
+        )
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
