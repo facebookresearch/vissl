78dd105c618cbc65187f1f1152d750b4ab227530
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-09-01 10:54:32 +0200

    Link fix

--- compvits/scripts/test_linear.sh
+++ compvits/scripts/test_linear.sh
@@ -25,8 +25,8 @@ python tools/run_distributed_engines.py \
     +config/compvits/data/test=in1k \
     +config/compvits/task=test_linear \
     config.CHECKPOINT.DIR=$dir \
-    +config.MODEL.WEIGHTS_INIT.PARAMS_FILE=../../vissl/checkpoints/${model}.pth \
-    +config.MODEL.WEIGHTS_INIT.STATE_DICT_KEY_NAME=model \
+    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=../../vissl/checkpoints/${model}.pth \
+    config.MODEL.WEIGHTS_INIT.STATE_DICT_KEY_NAME=model \
     config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.COMP.NAME=afterK \
     config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.COMP.PARAMS.K=$K \
     config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.SPLIT.NAME=precomputed_masks \
