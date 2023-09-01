eb16bad2adcb029865056d0664a0e086d93c16cc
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-09-01 09:35:28 +0200

    Link fix

--- compvits/scripts/test_linear.sh
+++ compvits/scripts/test_linear.sh
@@ -10,7 +10,7 @@ dir="logs/test_linear/M${M}/${model}/K$K"
 if [[ $model == "deitb" ]]; then
     head_cfg=mlp_768_1000
     trunk_cfg=deitb
-elif [[ $model == "vit_supervised" ]]; then
+elif [[ $model == "dino" ]]; then
     head_cfg=mlp_768_1000
     trunk_cfg=vitb
 else
@@ -25,7 +25,7 @@ python tools/run_distributed_engines.py \
     +config/compvits/data/test=in1k \
     +config/compvits/task=test_linear \
     +config.CHECKPOINT.DIR=$dir \
-    +config.MODEL.WEIGHTS_INIT.PARAMS_FILE=/Users/piotrwojcik/PycharmProjects/vissl/checkpoints/${model}.pth \
+    +config.MODEL.WEIGHTS_INIT.PARAMS_FILE=../../vissl/checkpoints/${model}.pth \
     +config.MODEL.WEIGHTS_INIT.STATE_DICT_KEY_NAME=model \
     config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.COMP.NAME=afterK \
     config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.COMP.PARAMS.K=$K \
