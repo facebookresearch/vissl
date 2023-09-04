f29a3ce3d43b5b972bbeb8bd5487e7f379f2dc05
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-09-04 12:11:31 +0200

    paths

--- compvits/scripts/extract_train_features.sh
+++ compvits/scripts/extract_train_features.sh
@@ -19,5 +19,5 @@ python tools/run_distributed_engines.py \
     engine_name=extract_features \
     config.CHECKPOINT.DIR=$dir \
     config.TEST_MODEL=False \
-    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=/home/jan.olszewski/git/vissl/checkpoints/${model}.pth \
+    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=checkpoints/trunk_only/${model}.pth \
     config.MODEL.WEIGHTS_INIT.STATE_DICT_KEY_NAME=model \
