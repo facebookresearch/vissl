45593da189635349958a0d65537d9185826e6a10
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-09-06 15:56:36 +0200

    added extract val feat

--- compvits/scripts/extract_val_features.sh
+++ compvits/scripts/extract_val_features.sh
@@ -4,7 +4,7 @@ model=$1
 
 echo extract_train_features: $model
 
-dir="logs/nearest_neighbor/train_features/${model}"
+dir="logs/nearest_neighbor/test_features/${model}"
 
 if [[ $model == "deitb" ]]; then
     trunk_cfg=deitb
@@ -15,9 +15,9 @@ fi
 python tools/run_distributed_engines.py \
     config=compvits/base \
     +config/compvits/model/trunk=$trunk_cfg \
-    +config/compvits/data/train=in1k \
+    +config/compvits/data/test=in1k \
     engine_name=extract_features \
-    config.TEST_MODEL=False \
+    config.TEST_ONLY=True \
     config.CHECKPOINT.DIR=$dir \
     config.MODEL.WEIGHTS_INIT.PARAMS_FILE=/data/pwojcik/vissl/checkpoints/trunk_only/${model}.pth \
     config.MODEL.WEIGHTS_INIT.STATE_DICT_KEY_NAME=model \
