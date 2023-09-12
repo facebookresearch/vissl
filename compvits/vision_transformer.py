73b7f896f53b92fdc1ddd9c15b3ceb408aaebcdf
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-09-12 11:03:48 +0200

    random M and K

--- compvits/vision_transformer.py
+++ compvits/vision_transformer.py
@@ -306,7 +306,8 @@ class VisionTransformer(nn.Module):
         K = random.randint(0, len(self.blocks))
         M = random.choice([2, 4, 8, 16, 3, 6, 9, 12])
         output = self.comp_forward_afterK(x, 'lastCLS', K, M)
-        print(output)
+
+        print(len(output))
         return output
 
     def get_last_selfattention(self, x):
