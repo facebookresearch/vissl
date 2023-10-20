94def58538d3c7037f5e093196494331eea1a2a2
Igor Sugak <sugak@meta.com>
2023-10-20 11:18:37 -0700

    replace numpy.bool with bool
    Summary:
    numpy.bool is long deprecated and removed starting numpy-1.20.0 [1]. This replaces all references with equivalent `bool` type using the following oneliner:
    ```
    rg -l np.bool deeplearning | grep .py | xargs perl -pi -e s,bnp.boolb,bool,
    ```
    1. https://numpy.org/devdocs/release/1.20.0-notes.html#deprecations
    
    Reviewed By: florazzz
    
    Differential Revision: D50426383
    
    fbshipit-source-id: 87ba599131b41abd16736386ef713fe31046545f

--- vissl/data/collators/cutmixup_collator.py
+++ vissl/data/collators/cutmixup_collator.py
@@ -369,7 +369,7 @@ class Mixup:
 
     def _params_per_elem(self, batch_size):
         lam = np.ones(batch_size, dtype=np.float32)
-        use_cutmix = np.zeros(batch_size, dtype=np.bool)
+        use_cutmix = np.zeros(batch_size, dtype=bool)
         if self.mixup_enabled:
             if self.mixup_alpha > 0.0 and self.cutmix_alpha > 0.0:
                 use_cutmix = np.random.rand(batch_size) < self.switch_prob
@@ -385,7 +385,7 @@ class Mixup:
                     self.mixup_alpha, self.mixup_alpha, size=batch_size
                 )
             elif self.cutmix_alpha > 0.0:
-                use_cutmix = np.ones(batch_size, dtype=np.bool)
+                use_cutmix = np.ones(batch_size, dtype=bool)
                 lam_mix = np.random.beta(
                     self.cutmix_alpha, self.cutmix_alpha, size=batch_size
                 )
